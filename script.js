055b0fa6dbd6062a91e5d52f90d32a12d2048a15
desame02 <angeldesantiagom02@gmail.com>
2024-06-21 22:47:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     event.preventDefault();
     alert('Formulario enviado. Gracias por contactarnos.');
 });
+
+document.getElementById('calc-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const aireadores = parseInt(document.getElementById('aireadores').value);
+    const bombas = parseInt(document.getElementById('bombas').value);
+    const precioAireador = 199.49;
+    const precioBomba = 300.40;
+    const total = (aireadores * precioAireador) + (bombas * precioBomba);
+    document.getElementById('resultado').innerText = `Precio estimado: $${total.toFixed(2)}`;
+});
